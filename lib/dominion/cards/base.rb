c7a1f1340c13a859f9b65b5ff8dfe6bad1799096
Shane Emmons <semmons99@gmail.com>
2010-08-03 13:46:54 -0400

    add #initialize, #owner and #setup to Dominion::Cards::Base

--- lib/dominion/cards/base.rb
+++ lib/dominion/cards/base.rb
@@ -1,6 +1,18 @@
 module Dominion
   module Cards
     class Base
+      attr_reader :owner
+
+      def initialize(game, owner = nil)
+        @game  = game
+        @owner = owner
+        setup
+      end
+
+      def setup
+        raise NotImplementedError, "#setup must be implemented"
+      end
+
       def name
         raise NotImplementedError, "#name must be implemented"
       end
