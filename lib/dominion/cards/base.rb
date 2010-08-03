b3ba74d3cb31ddde08d74f7703b588e82c72d7fc
Shane Emmons <semmons99@gmail.com>
2010-08-03 11:47:27 -0400

    add #cost to Dominion::Cards::Base

--- lib/dominion/cards/base.rb
+++ lib/dominion/cards/base.rb
@@ -8,6 +8,10 @@ module Dominion
       def type
         raise NotImplementedError, "#type must be implemented"
       end
+
+      def cost
+        raise NotImplementedError, "#cost must be implemented"
+      end
     end
   end
 end
