b3cdac5cc802c86ffd44ef61f3b5b2712d67306d
Shane Emmons <semmons99@gmail.com>
2010-08-03 14:57:55 -0400

    implement Dominion::Cards::Cellar

---
+++ lib/dominion/cards/action_cards/cellar.rb
@@ -0,0 +1,20 @@
+require 'dominion/cards/action_cards/action_base'
+
+module Dominion
+  module Cards
+    class Cellar < Dominion::Cards::ActionBase
+      def setup
+        super
+        @name    = "Cellar"
+        @cost    = 2
+        @actions = 1
+      end
+
+      def special_action
+        n = @game.ask('how many card to discard?')
+        owner.hand.discard(n)
+        owner.draw(n)
+      end
+    end
+  end
+end
